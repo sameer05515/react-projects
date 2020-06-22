0aad50c565678816fe96ec4afd239916860f8197
Premendra Kumar <premendra.kumar@indiabulls.com>
2020-06-22 15:39:50 +0530

    committing at Mon 06/22/2020 15:39:46.65

--- react-inner-scroll/src/App.js
+++ react-inner-scroll/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
 import ScrollView, { ScrollElement } from "./scroller";
+import topicMgmtInstance from './axios';
 
 import items from "./data";
 
 class App extends Component {
+  componentDidMount(){
+    topicMgmtInstance.get('/groups')
+    .then(response =>{
+      console.log(response);
+      this.setState({
+        views:response.data
+      });
+    })
+  }
+
+  state={
+    persons:[
+        {name:'Premendra',age:29},
+        {name:'Vandana',age:26},
+        {name:'Narendra',age:27}
+    ],
+    views:[],
+    counter:1    
+};
+
+
   scrollTo = (name) => {
     this._scroller.scrollTo(name);
   }
@@ -22,6 +44,24 @@ class App extends Component {
                   <div className="item">
                     <img src={image} />
                     {name}
+                  </div>
+                </ScrollElement>
+              );
+            })}
+          </div>
+        </ScrollView>
+
+        {
+          this.state.views.map(({id,title}) => <button onClick={() => this.scrollTo(id)}>{title}</button>)
+        }
+        <ScrollView ref={scroller => this._scroller = scroller}>
+          <div className="scroller">
+            {this.state.views.map(({ id, title,description }) => {
+              return (
+                <ScrollElement name={id}>
+                  <div className="item">
+                    
+                   <strong>{title}</strong>  : {description}
                   </div>
                 </ScrollElement>
               );

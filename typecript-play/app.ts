313d6f9aa87ca469e82e62eba88d6a10e343d87a
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-01-08 08:40:22 +0530

    committing at Fri Jan  8 08:40:15 IST 2021

--- typecript-play/app.ts
+++ typecript-play/app.ts
@@ -1,1 +1,9 @@
-console.log('first line.....');
+function add(n1: number, n2: number){
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2;
+
+const result=add(number1,number2);
+console.log(result);

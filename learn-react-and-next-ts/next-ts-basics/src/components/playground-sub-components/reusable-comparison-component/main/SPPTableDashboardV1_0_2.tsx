aa25beb70a2f30b6d526eb771e018d853472766a
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-10-15 17:51:32 +0530

    commit

--- learn-react-and-next-ts/next-ts-basics/src/components/playground-sub-components/reusable-comparison-component/main/SPPTableDashboardV1_0_2.tsx
+++ learn-react-and-next-ts/next-ts-basics/src/components/playground-sub-components/reusable-comparison-component/main/SPPTableDashboardV1_0_2.tsx
@@ -1,36 +1,12 @@
-import React from "react";
-import { styles } from "../common/styles/styles_v1_0_1";
 import {
-  type CDRowItemType,
-  type ComparisonDataType,
-  comparisonData,
+    comparisonData
 } from "../common/data/data_v1_0_2";
-import TableHeader from "../sub-components/table-header/TableHeaderV1_0_1";
-import TableRow from "../sub-components/table-row/TableRowV1_0_1";
 import SPPTableV1_0_2 from "../sub-components/comparison-component/SPPTableV1_0_2";
-
-
-
-// Reusable Comparison Component (handles N items)
-// const ComparisonComponent = ({
-//   data,
-// }: {
-//   data: ComparisonDataType<string>;
-// }) => (
-//   <table style={styles.table}>
-//     <TableHeader headers={data.headers} />
-//     <tbody>
-//       {data.differences.map((item, index) => (
-//         <TableRow key={index} aspect={item.aspect} values={item.values} />
-//       ))}
-//     </tbody>
-//   </table>
-// );
 
 // Usage example
 const SPPTableDashboardV1_0_2 = () => (
   <div>
-    <h1>{comparisonData.title}</h1>
+    
     <SPPTableV1_0_2 data={comparisonData} />
   </div>
 );
--- learn-react-and-next-ts/next-ts-basics/src/components/playground-sub-components/reusable-comparison-component/sub-components/comparison-component/SPPTableV1_0_2.tsx
+++ learn-react-and-next-ts/next-ts-basics/src/components/playground-sub-components/reusable-comparison-component/sub-components/comparison-component/SPPTableV1_0_2.tsx
@@ -4,19 +4,18 @@ import TableHeader from "../table-header/TableHeaderV1_0_1";
 import TableRow from "../table-row/TableRowV1_0_1";
 
 // Reusable Comparison Component (handles N items)
-const SPPTableV1_0_2 = ({
-  data,
-}: {
-  data: ComparisonDataType<string>;
-}) => (
-  <table style={styles.table}>
-    <TableHeader headers={data.headers} />
-    <tbody>
-      {data.differences.map((item, index) => (
-        <TableRow key={index} aspect={item.aspect} values={item.values} />
-      ))}
-    </tbody>
-  </table>
+const SPPTableV1_0_2 = ({ data }: { data: ComparisonDataType<string> }) => (
+  <>
+    <h1>{data.title}</h1>
+    <table style={styles.table}>
+      <TableHeader headers={data.headers} />
+      <tbody>
+        {data.differences.map((item, index) => (
+          <TableRow key={index} aspect={item.aspect} values={item.values} />
+        ))}
+      </tbody>
+    </table>
+  </>
 );
 
 export default SPPTableV1_0_2;

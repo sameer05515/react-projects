5a1d7f2cf2ec1d76ee994dd9b943724cc12f333a
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-10-15 18:27:41 +0530

    commit

--- learn-react-and-next-ts/next-ts-basics/src/common/utils/app-component-constants.tsx
+++ learn-react-and-next-ts/next-ts-basics/src/common/utils/app-component-constants.tsx
@@ -98,25 +98,6 @@ const moduleWithPurposes: ComponentModuleDetails[] = [
                     - Support for sorting and filtering rows based on column values.
         
         ================================================================
-
-        Tasks status in before version 'SPPTableV1_1_0' :
-            [Completed] - Implement a basic table component.
-            [Planned] - Enhance it further to meet the specified targets.
-              - [Planned] - Common Components Extraction
-                TableHeader: A reusable component for rendering table headers.
-                TableRow: A reusable component for rendering each row.
-                TableCell: A reusable component for rendering each row.
-              - [Planned] - Styles: A separate object for holding the common styles.
-              - [Planned] - Extract data and keep in a seperate tsx file for re-usability purpose
-            [Planned] - Add support for validation in editable cells (optional).
-            [Planned] - Include export and import options for the table data (optional).
-            [Planned] - Add row and column operations like adding/removing rows or columns dynamically.
-            [Planned] - Support responsive design to ensure table works across different devices.
-            [Planned] - Allow table data to be stored and retrieved via local storage or external API.
-            [Planned] - Add cell-level formatting options such as font style, text alignment, and background color.
-
-        ================================================================
-
     `,
   },
 ];
@@ -903,17 +884,17 @@ const componentMapWithPurposes: Record<string, ComponentDetails> = {
             - [Completed][V1_0_0] - Implement a basic table component.
             - [Planned] - Enhance it further to meet the specified targets.
               - [Planned] - Common Components Extraction
-                - [Completed][V1_0_0] TableHeaderV1_0_0: A reusable component for rendering table headers.
-                - [Completed][V1_0_0] TableRowV1_0_0: A reusable component for rendering each row.
+                - [Completed]: [V1_0_0]: TableHeaderV1_0_0: A reusable component for rendering table headers.
+                - [Completed]: [V1_0_0]: TableRowV1_0_0: A reusable component for rendering each row.
                 - [Planned] TableCell: A reusable component for rendering each row.
               - [Planned][V1_0_0] - Styles: A separate object for holding the common styles. 
                 - [Completed][V1_0_0] created a file 'reusable-comparison-component/common/styles/styles_v1_0_0.tsx'
                 - [Planned] - create styles object for subsequent component evolution
               - [Planned] - Extract data and keep in a seperate tsx file for re-usability purpose
                 - [Completed][V1_0_0] - Data extracted and put to a common 'reusable-comparison-component/common/data/data_v1_0_0.tsx' file
-                - [Completed][V1_0_1] - upgraded to 'data_v1_0_1.tsx' to hold multiple difference objects. changed hardcoded 'ArrayList' and 'LinkedList' headers to general 'left' and 'right' in 'CDRowItemType' type
-                - [Completed][V1_0_2] - Generic CDRowItemType<T> and Generic ComparisonDataType<T> to support 'Generic row value' and 2-Item Comparisons
-                - [Completed][V1_0_2] - Generic CDRowItemType<T> and Generic ComparisonDataType<T> to support 'Generic row value' and N-Item Comparisons
+                - [Completed]: [V1_0_1]: - upgraded to 'data_v1_0_1.tsx' to hold multiple difference objects. changed hardcoded 'ArrayList' and 'LinkedList' headers to general 'left' and 'right' in 'CDRowItemType' type
+                - [Completed]: [V1_0_2]: - Generic CDRowItemType<T> and Generic ComparisonDataType<T> to support 'Generic row value' and 2-Item Comparisons
+                - [Completed]: [V1_0_2]: - Generic CDRowItemType<T> and Generic ComparisonDataType<T> to support 'Generic row value' and N-Item Comparisons
             - [Planned] - Add a context, to pass common configurations. This way we can refrain from uneccesary 'prop-drilling'
             - [Planned] - Add support for validation in editable cells (optional).
             - [Planned] - Include export and import options for the table data (optional).

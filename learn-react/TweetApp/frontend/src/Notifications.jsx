4ea11510aac771347c71871318d5055b75c1a07d
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-10-20 21:27:18 +0530

    commit

--- learn-react/TweetApp/frontend/src/Notifications.jsx
+++ learn-react/TweetApp/frontend/src/Notifications.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchMemoryMapByUniqueId } from "./redux/slices/memoryMapSlice";
+import React from "react";
 import {
     SmartPreviewer,
     availableOutputTypes as SupportedTextFormats,
@@ -10,7 +8,7 @@ import useDataFetching from "./common/hooks/useDataFetching";
 
 const Notifications = () => {
     const isDarkMode = false;
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
     const url = `${BACKEND_APPLICATION_BASE_URL}/memory-maps/cd6bb190-0e56-4e3a-8f01-748c8d05d9d4`;
     const { data, loading, error, refetch } = useDataFetching(url);
 
@@ -26,15 +24,13 @@ const Notifications = () => {
                 paddingTop: "5px",
             }}
         >
-            <h1>
-                <SmartPreviewer
-                    data={{
-                        content: data?.name || "",
-                        textOutputType: SupportedTextFormats.MARKDOWN,
-                    }}
+            <SmartPreviewer
+                data={{
+                    content: data?.name || "",
+                    textOutputType: SupportedTextFormats.MARKDOWN,
+                }}
                 markdownStyles={{ fontSize: "25px" }}
-                />
-            </h1>
+            />
             <SmartPreviewer
                 data={{
                     content: data?.skeleton || "",

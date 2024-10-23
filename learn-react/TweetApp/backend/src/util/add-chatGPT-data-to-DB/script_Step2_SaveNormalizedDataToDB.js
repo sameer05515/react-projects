a161b2bafb5304787549bec87eb77782ed326eb3
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-10-23 14:25:19 +0530

    commit

--- learn-react/TweetApp/backend/src/util/add-chatGPT-data-to-DB/script_Step2_SaveNormalizedDataToDB.js
+++ learn-react/TweetApp/backend/src/util/add-chatGPT-data-to-DB/script_Step2_SaveNormalizedDataToDB.js
@@ -7,6 +7,7 @@ const {
 } = require("../../routes/ChatGPTConversation.model");
 const { CGPTFileNames } = require("./constants");
 const { fetchJsonData } = require("./util");
+const fs = require("fs").promises; // Use promises API for fs
 
 const metadata = {
     dateOfExecution: "23-Oct-2023",
@@ -30,74 +31,100 @@ const metadata = {
     `,
 };
 
+const outputFolderLocation = "D:/GIT/my-backup/ChatGPTNormalizedData";
+
+
+// Function to read a JSON file
+const readJsonFile = async (fileName) => {
+
+    const outputFile = `${outputFolderLocation}/ND_${fileName}.json`;
+
+    try {
+        // Read file contents
+        const data = await fs.readFile(outputFile, "utf8");
+
+        // Parse the JSON data
+        const jsonData = JSON.parse(data);
+
+        // Return the parsed data
+        return jsonData;
+    } catch (error) {
+        console.error("Error reading JSON file:", error);
+        throw error; // Re-throw error for further handling if needed
+    }
+};
+
 // 1. Save hardcoded chatGPT file names
 async function saveHardcodedChatGPTFileNames() {
     try {
-        for (const category of CGPTFileNames.filter((c) => c.isLatest)) {
+        for (const category of CGPTFileNames.filter(d=>d.isLatest)) {
+            const data = await readJsonFile(category.name);
             const newTag = new CGPTFile({
-                uniqueId: uuidv4(),
-                name: category.name,
-                location: category.location,
-                descriptions: [],
-                heading: category.name, // Assuming you want to copy smartContent
-                parentId: "", // Or whatever logic is required
-                createdDate: category.createdDate,
-                isLatest: category.isLatest,
+                // uniqueId: uuidv4(),
+                // name: category.name,
+                // location: category.location,
+                // descriptions: [],
+                // heading: category.name, // Assuming you want to copy smartContent
+                // parentId: "", // Or whatever logic is required
+                // createdDate: category.createdDate,
+                // isLatest: category.isLatest,
+                ...data
             });
 
             const savedTag = await newTag.save();
-            await fetchandSaveConvAndMessages(savedTag);
+            // await fetchandSaveConvAndMessages(savedTag);
+            console.log(`Data saved successfully at: ${savedTag.name}`);
         }
     } catch (err) {
         console.error("Error in saveHardcodedChatGPTFileNames:", err);
     }
 }
 
-async function fetchandSaveConvAndMessages(savedTag) {
-    try {
-        const convArr = await fetchJsonData(savedTag.location);
-        for (const conv of convArr) {
-            const newConv = new CGPTConversation({
-                uniqueId: uuidv4(),
-                name: conv.name,
-                createdDate: conv.createdOn,
-                updatedDate: conv.updatedOn,
-                descriptions: [],
-                heading: conv.name, // Assuming you want to copy smartContent
-                parentId: "", // Or whatever logic is required
-                linkedCGPTFileId: savedTag.uniqueId,
-            });
-            const savedConv = await newConv.save();
-            await saveMessages(savedConv, conv.messages);
-        }
-    } catch (err) {
-        console.error("Error in fetchandSaveConvAndMessages:", err);
-    }
-}
+// async function fetchandSaveConvAndMessages(savedTag) {
+//     try {
+//         const convArr = await fetchJsonData(savedTag.location);
+//         for (const conv of convArr) {
+//             const newConv = new CGPTConversation({
+//                 uniqueId: uuidv4(),
+//                 name: conv.name,
+//                 createdDate: conv.createdOn,
+//                 updatedDate: conv.updatedOn,
+//                 descriptions: [],
+//                 heading: conv.name, // Assuming you want to copy smartContent
+//                 parentId: "", // Or whatever logic is required
+//                 linkedCGPTFileId: savedTag.uniqueId,
+//             });
+//             const savedConv = await newConv.save();
+//             await saveMessages(savedConv, conv.messages);
+//         }
+//     } catch (err) {
+//         console.error("Error in fetchandSaveConvAndMessages:", err);
+//     }
+// }
 
-async function saveMessages(savedConv, messages) {
-    try {
-        for (const msg of messages) {
-            const newMsg = new CGPTMessage({
-                uniqueId: uuidv4(),
-                oldId: msg.oldId,
-                name: msg.oldId + "_" + savedConv.uniqueId,
-                author: msg.author,
-                linkedCGPTConvId: savedConv.uniqueId,
-                descriptions: [
-                    {
-                        content: msg.text,
-                        textOutputType: "markdown",
-                        textInputType: "TextArea",
-                    },
-                ],
-            });
-            await newMsg.save();
-        }
-    } catch (err) {
-        console.error("Error in fetchandSaveConvAndMessages:", err);
-    }
-}
+// async function saveMessages(savedConv, messages) {
+//     try {
+//         for (const msg of messages) {
+//             const newMsg = new CGPTMessage({
+//                 uniqueId: uuidv4(),
+//                 oldId: msg.oldId,
+//                 name: msg.oldId + "_" + savedConv.uniqueId,
+//                 author: msg.author,
+//                 linkedCGPTConvId: savedConv.uniqueId,
+//                 descriptions: [
+//                     {
+//                         content: msg.text,
+//                         textOutputType: "markdown",
+//                         textInputType: "TextArea",
+//                     },
+//                 ],
+//             });
+//             await newMsg.save();
+//         }
+//     } catch (err) {
+//         console.error("Error in fetchandSaveConvAndMessages:", err);
+//     }
+// }
 
 async function main() {
     try {

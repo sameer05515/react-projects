6cd01d4cb4bd188d6a9a3756c5e6eaead3853285
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2024-12-31 20:19:27 +0530

    commit

--- learn-react/TweetApp/frontend/src/common/hooks/useConsolidated/v4.jsx
+++ learn-react/TweetApp/frontend/src/common/hooks/useConsolidated/v4.jsx
@@ -176,6 +176,14 @@ import {
  * experience, this version aims to be a go-to utility for developers building modern React applications.
  */
 
+/**
+ * List of bug-fixes (found in [.releases/v400.jsx](./releases/v400.jsx)) done
+ * 
+ * 1. **Bug**: Even if validator function is throwing exception, the promise is starting to execute in background.
+ *  - **RCA**: the validation for determining given `apiRequest` argument was being done, before executing `validatorFn`
+ * 
+*/
+
 const useConsolidated = () => {
   const dispatch = useDispatch();
 

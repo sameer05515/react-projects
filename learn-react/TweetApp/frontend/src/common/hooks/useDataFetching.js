335075b10bd1eee61315cfc262fc35cb20f7eb6c
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-01 15:58:04 +0530

    commit

--- learn-react/TweetApp/frontend/src/common/hooks/useDataFetching.js
+++ learn-react/TweetApp/frontend/src/common/hooks/useDataFetching.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 
 const prepareErrorMessage = (error, defaultMessage) => {
   if (!error) return defaultMessage;

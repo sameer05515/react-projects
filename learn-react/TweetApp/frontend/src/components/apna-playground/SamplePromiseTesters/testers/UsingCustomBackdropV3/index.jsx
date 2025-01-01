4e2a72c48581a465c7b6491c60c81df8bb1b393d
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-01-02 02:47:07 +0530

    commit

--- learn-react/TweetApp/frontend/src/components/apna-playground/SamplePromiseTesters/testers/UsingCustomBackdropV3/index.jsx
+++ learn-react/TweetApp/frontend/src/components/apna-playground/SamplePromiseTesters/testers/UsingCustomBackdropV3/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { convertToYesNo } from "../../utils";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -6,22 +6,41 @@ import {
   v3,
 } from "../../../../../redux/slices/backdropSlice";
 import CustomBackdropV3 from "../../../../../common/components/CustomBackdrop/v3";
-import { delayForMS } from "../../../sample-promises";
+import { createIntervalPromise, delayForMS } from "../../../sample-promises";
 
 const UsingCustomBackdropV3 = () => {
   const dispatch = useDispatch();
   const isActive = useSelector(selectIsCustomBackdropV3Active);
-  const handleClick = async () => {
+  const handleClick = useCallback(async () => {
     dispatch(v3.show());
     await delayForMS(1000);
     dispatch(v3.hide());
-  };
+  }, [dispatch]);
+
+  const handleUpdateTitle = useCallback(async () => {
+    try {
+      let itration = 0;
+      await createIntervalPromise(
+        () => {
+          console.log(`Iteration Number: ${++itration}`);
+        },
+        3000,
+        5
+      );
+      console.log("Interval completed!");
+    } catch (error) {
+      console.error("Error during interval:", error);
+    }
+  }, []);
+
   return (
     <div>
       <h1>UsingCustomBackdropV3</h1>
       <h2>CustomBackdropV3 is active: {convertToYesNo(isActive)}</h2>
       <br />
       <button onClick={handleClick}>Show BackdropV3</button>
+      <br />
+      <button onClick={handleUpdateTitle}>Update Title of BackdropV3</button>
 
       <CustomBackdropV3 />
     </div>

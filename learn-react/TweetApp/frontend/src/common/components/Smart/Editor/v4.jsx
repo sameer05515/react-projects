70422fddab6acc5748a81604b633da4cbe4116d5
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-01 22:16:41 +0530

    commit

--- learn-react/TweetApp/frontend/src/common/components/Smart/Editor/v4.jsx
+++ learn-react/TweetApp/frontend/src/common/components/Smart/Editor/v4.jsx
@@ -8,11 +8,17 @@ import JSONDataViewer from "../../json-data-viewer/JSONDataViewer";
 import { getKeyName, inputOutputMapping, SupportedInputComponents, SupportedOutFormats } from "../common/utils.v4";
 import SmartPreviewer from "../Previewer/v4";
 
+const FormError = ({ error }) => {
+  if (!error) return null;
+  return <div className="alert alert-danger mt-2">{error}</div>;
+};
+
 const SmartEditorV4 = ({ initialValue, preview: previewInitialValue = true, onChange = () => {}, onError = () => {} }) => {
   const textareaRef = useRef(null);
 
   const [selectedOutputType, setSelectedOutputType] = useState(getKeyName(initialValue?.textOutputType, initialValue?.textInputType));
   const [showPreview, setShowPreview] = useState(previewInitialValue);
+  const [error, setError] = useState("");
 
   const [formData, setFormData] = useState({
     content: initialValue?.content || "",
@@ -55,41 +61,57 @@ const SmartEditorV4 = ({ initialValue, preview: previewInitialValue = true, onCh
   //   // onChange(formData);
   // }, [formData.content, formData.textOutputType]);
 
-  const handleFormUpdate = (content, textOutputType, textInputType) => {
+  const handleFormUpdate = (newContent, newTextOutputType, newTextInputType) => {
     // const { textOutputType, content } = formData;
 
-    let error = "";
-    if (textOutputType === SupportedOutFormats.YAML && content) {
+    let validationError = "";
+    if (newTextOutputType === SupportedOutFormats.YAML && newContent) {
       try {
-        yaml.load(content);
+        yaml.load(newContent);
       } catch (e) {
-        error = e.mark ? `Error parsing YAML at line ${e.mark.line + 1}: ${e.message}` : `Error parsing YAML: ${e.message}`;
+        validationError = e.mark ? `Error parsing YAML at line ${e.mark.line + 1}: ${e.message}` : `Error parsing YAML: ${e.message}`;
       }
     }
 
-    if (textOutputType === SupportedOutFormats.TIS_to_SKELETON && content) {
-      const { isValid, message } = buildTree(content);
-      if (!isValid) error = message;
+    if (newTextOutputType === SupportedOutFormats.TIS_to_SKELETON && newContent) {
+      const { isValid, message } = buildTree(newContent);
+      if (!isValid) validationError = message;
     }
 
-    if (!content.trim()) error = "Content is empty";
+    if (!newContent?.trim()) {
+      validationError = "Content is empty";
+    }
 
     if (textareaRef.current) {
       textareaRef.current.style.height = "auto";
       textareaRef.current.style.height = `${textareaRef.current.scrollHeight * 1.3}px`;
     }
 
+    setError(validationError);
+
+    setFormData((prev) => ({
+      ...prev,
+      content: newContent ? newContent : prev.content,
+      textOutputType: newTextOutputType && prev.textOutputType !== newTextOutputType ? newTextOutputType : prev.textOutputType,
+      textInputType: newTextInputType && prev.textInputType !== newTextInputType ? newTextInputType : prev.textInputType,
+    }));
+
     // onError(error);
     // onChange(formData);
   };
   // , [formData.content, formData.textOutputType]);
 
-  const handleChangeOutputTypes = (event) => {
-    setSelectedOutputType(event.target.value);
-    const { textInputType, textOutputType } = inputOutputMapping[selectedOutputType];
-    if (formData.textOutputType !== textOutputType || formData.textInputType !== textInputType) {
-      setFormData((prev) => ({ ...prev, textInputType, textOutputType }));
+  const handleChangeOutputTypes = (newOutputType) => {
+    if (!newOutputType) {
+      console.error("Invalid newOutputType", newOutputType);
+      return;
     }
+    setSelectedOutputType(newOutputType);
+    const { textInputType, textOutputType } = inputOutputMapping[selectedOutputType];
+    handleFormUpdate(null, textOutputType, textInputType);
+    // if (formData.textOutputType !== textOutputType || formData.textInputType !== textInputType) {
+    //   setFormData((prev) => ({ ...prev, textInputType, textOutputType }));
+    // }
   };
   // const handleInputChange = (e) => setFormData((prev) => ({ ...prev, content: e.target.value }));
   // const handleEditorChange = (event, editor) => setFormData((prev) => ({ ...prev, content: editor.getData() }));
@@ -99,7 +121,8 @@ const SmartEditorV4 = ({ initialValue, preview: previewInitialValue = true, onCh
       console.error("Invalid content", content);
       return;
     }
-    setFormData((prev) => ({ ...prev, content: content }));
+    handleFormUpdate(content);
+    // setFormData((prev) => ({ ...prev, content: content }));
   };
 
   return (
@@ -107,7 +130,11 @@ const SmartEditorV4 = ({ initialValue, preview: previewInitialValue = true, onCh
       <label htmlFor="outputType" style={styles.labelStyle}>
         Select Output Type:
       </label>
-      <select value={selectedOutputType} title={inputOutputMapping[selectedOutputType]?.detailedName || ""} onChange={handleChangeOutputTypes}>
+      <select
+        value={selectedOutputType}
+        title={inputOutputMapping[selectedOutputType]?.detailedName || ""}
+        onChange={(event) => handleChangeOutputTypes(event.target.value)}
+      >
         {Object.keys(inputOutputMapping).map((outputType) => (
           <option key={outputType} value={outputType}>
             {outputType.replace(/_/g, " ")}
@@ -145,6 +172,8 @@ const SmartEditorV4 = ({ initialValue, preview: previewInitialValue = true, onCh
           />
         </div>
       )}
+
+      <FormError error={error} />
 
       {formData.content && (
         <div>

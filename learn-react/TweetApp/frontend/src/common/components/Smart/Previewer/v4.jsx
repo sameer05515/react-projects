cd38303e172ae2baf5bdfd100b64965a5e83ae6c
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-02 02:58:38 +0530

    commit

--- learn-react/TweetApp/frontend/src/common/components/Smart/Previewer/v4.jsx
+++ learn-react/TweetApp/frontend/src/common/components/Smart/Previewer/v4.jsx
@@ -1,47 +1,18 @@
-import yaml from "js-yaml";
 import React, { useMemo } from "react";
 import CopyButton from "../../../../components/memory-maps/copy-to-clipboard/CopyButton";
-import { isValidString } from "../../../service/basic-validations";
-import { addUniqueIdsToTree } from "../../../util/id-adder-util";
-import { buildTree } from "../../../util/indentation-based-string-parser-to-tree-data";
 import JSONDataViewer from "../../json-data-viewer/JSONDataViewer";
 import MarkdownComponent from "../../markdown-component/MarkdownComponent";
 import Tree from "../../tree-viewer/TreeViewer";
 import { SupportedOutFormats } from "../common/utils.v4";
+import { getSmartPreviewerProcessedData } from "../common/utils.v4";
 
 const debug = false;
 
-const getSPProcessedData = (data) => {
-  const content = isValidString(data?.content) ? data.content : "";
-  const textOutputType = Object.values(SupportedOutFormats).includes(data?.textOutputType)
-    ? data.textOutputType
-    : SupportedOutFormats.TEXT;
 
-  let yamlProcessedData = null;
-  let resultData = [];
-  let errorMessage = "";
-
-  if (content && [SupportedOutFormats.YAML, SupportedOutFormats.YAML_to_SKELETON].includes(textOutputType)) {
-    try {
-      yamlProcessedData = yaml.load(content);
-      if (textOutputType === SupportedOutFormats.YAML_to_SKELETON) resultData = yamlProcessedData;
-    } catch (e) {
-      errorMessage = e.mark
-        ? `Error parsing YAML at line ${e.mark.line + 1}: ${e.message}`
-        : `Error parsing YAML: ${e.message}`;
-    }
-  } else if (textOutputType === SupportedOutFormats.TIS_to_SKELETON && content) {
-    const { data: skeletonData, isValid, message } = buildTree(content);
-    if (isValid) resultData = addUniqueIdsToTree(skeletonData);
-    else errorMessage = message || "Missing error message";
-  }
-
-  return { content, textOutputType, yamlProcessedData, resultData, errorMessage };
-};
 
 const SmartPreviewerV4 = ({ data }) => {
   const { content, textOutputType, yamlProcessedData, resultData, errorMessage } = useMemo(
-    () => getSPProcessedData(data),
+    () => getSmartPreviewerProcessedData(data),
     [data]
   );
 
--- learn-react/TweetApp/frontend/src/common/components/Smart/common/utils.v4.js
+++ learn-react/TweetApp/frontend/src/common/components/Smart/common/utils.v4.js
@@ -1,3 +1,9 @@
+import yaml from "js-yaml";
+import React from "react";
+import { isValidString } from "../../../service/basic-validations";
+import { addUniqueIdsToTree } from "../../../util/id-adder-util";
+import { buildTree } from "../../../util/indentation-based-string-parser-to-tree-data";
+
 export const SupportedOutFormats = {
   HTML: "html",
   YAML: "yaml",
@@ -39,7 +45,8 @@ const inputOutputMapping = {
     textInputType: SupportedInputComponents.textArea,
   },
   SKELETON_From_TIS: {
-    detailedName: "Tabbed Indented String (TIS) format text, input from RawText. Will show parsing error if invalid YAML text given",
+    detailedName:
+      "Tabbed Indented String (TIS) format text, input from RawText. Will show parsing error if invalid YAML text given",
     textOutputType: SupportedOutFormats.TIS_to_SKELETON,
     textInputType: SupportedInputComponents.textArea,
   },
@@ -47,7 +54,9 @@ const inputOutputMapping = {
 
 export const getKeyName = (textOutputType, textInputType) =>
   Object.keys(inputOutputMapping).find(
-    (key) => inputOutputMapping[key].textOutputType === textOutputType && inputOutputMapping[key].textInputType === textInputType
+    (key) =>
+      inputOutputMapping[key].textOutputType === textOutputType &&
+      inputOutputMapping[key].textInputType === textInputType
   ) || "";
 
 const getDetailedNameForKey = (key = "") => {
@@ -88,3 +97,33 @@ export const getComboOptions = () => (
     ))}
   </>
 );
+
+//=========================================
+
+export const getSmartPreviewerProcessedData = (data) => {
+  const content = isValidString(data?.content) ? data.content : "";
+  const textOutputType = Object.values(SupportedOutFormats).includes(data?.textOutputType)
+    ? data.textOutputType
+    : SupportedOutFormats.TEXT;
+
+  let yamlProcessedData = null;
+  let resultData = [];
+  let errorMessage = "";
+
+  if (content && [SupportedOutFormats.YAML, SupportedOutFormats.YAML_to_SKELETON].includes(textOutputType)) {
+    try {
+      yamlProcessedData = yaml.load(content);
+      if (textOutputType === SupportedOutFormats.YAML_to_SKELETON) resultData = yamlProcessedData;
+    } catch (e) {
+      errorMessage = e.mark
+        ? `Error parsing YAML at line ${e.mark.line + 1}: ${e.message}`
+        : `Error parsing YAML: ${e.message}`;
+    }
+  } else if (textOutputType === SupportedOutFormats.TIS_to_SKELETON && content) {
+    const { data: skeletonData, isValid, message } = buildTree(content);
+    if (isValid) resultData = addUniqueIdsToTree(skeletonData);
+    else errorMessage = message || "Missing error message";
+  }
+
+  return { content, textOutputType, yamlProcessedData, resultData, errorMessage };
+};

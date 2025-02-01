bad5f20e4721608c6f93c59041f257cd3baf20d5
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-01 17:28:47 +0530

    commit

--- learn-react/TweetApp/backend/src/routes/common/SmartContent.schema.v2.js
+++ learn-react/TweetApp/backend/src/routes/common/SmartContent.schema.v2.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 
+const SupportedOutFormats = {
+  HTML: "html",
+  YAML: "yaml",
+  MARKDOWN: "markdown",
+  TEXT: "text",
+  TIS_to_SKELETON: "skeleton",
+  YAML_to_SKELETON: "yaml_to_skeleton",
+};
+
+const SupportedInputComponents = {
+  textArea: "TextArea",
+  ckEditor: "CKEditor",
+};
+
 const smartContentSchema = new mongoose.Schema({
   content: {
     type: String,
@@ -7,13 +21,17 @@ const smartContentSchema = new mongoose.Schema({
   },
   textOutputType: {
     type: String,
-    enum: ['html', 'yaml', 'markdown', 'skeleton', 'text'], // Assuming possible values are 'html' and 'plaintext'
+    // enum: ['html', 'yaml', 'markdown', 'skeleton', 'text'], // Assuming possible values are 'html' and 'plaintext'
+    enum: Object.values(SupportedOutFormats),
     required: true,
+    default: SupportedOutFormats.TEXT,
   },
   textInputType: {
     type: String,
-    enum: ['CKEditor', 'TextArea'],
+    // enum: ['CKEditor', 'TextArea'],
+    enum: Object.values(SupportedInputComponents),
     required: true,
+    default: SupportedInputComponents.textArea,
   }
 });
 

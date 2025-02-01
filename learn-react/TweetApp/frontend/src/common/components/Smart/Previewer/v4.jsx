faf41db7f822f43e0ce51e9750e32a348f835c2a
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-02 03:01:45 +0530

    commit

--- learn-react/TweetApp/frontend/src/common/components/Smart/Previewer/v4.jsx
+++ learn-react/TweetApp/frontend/src/common/components/Smart/Previewer/v4.jsx
@@ -3,8 +3,7 @@ import CopyButton from "../../../../components/memory-maps/copy-to-clipboard/Cop
 import JSONDataViewer from "../../json-data-viewer/JSONDataViewer";
 import MarkdownComponent from "../../markdown-component/MarkdownComponent";
 import Tree from "../../tree-viewer/TreeViewer";
-import { SupportedOutFormats } from "../common/utils.v4";
-import { getSmartPreviewerProcessedData } from "../common/utils.v4";
+import { SupportedOutFormats, getSmartPreviewerProcessedData } from "../common/utils.v4";
 
 const debug = false;
 
@@ -14,41 +13,53 @@ const SmartPreviewerV4 = ({ data }) => {
     [data]
   );
 
+  const renderContent = () => {
+    switch (textOutputType) {
+      case SupportedOutFormats.TEXT:
+        return <pre>{content}</pre>;
+
+      case SupportedOutFormats.HTML:
+        return <div style={{ whiteSpace: "pre-wrap" }} dangerouslySetInnerHTML={{ __html: content }} />;
+
+      case SupportedOutFormats.MARKDOWN:
+        return <MarkdownComponent markdownText={content} />;
+
+      case SupportedOutFormats.YAML:
+        return (
+          <>
+            <pre>{JSON.stringify(yamlProcessedData, null, 2)}</pre>
+            {!!errorMessage && <span style={{ color: "red" }}>{errorMessage}</span>}
+          </>
+        );
+
+      case SupportedOutFormats.TIS_to_SKELETON:
+        return resultData.length > 0 ? (
+          <>
+            <Tree
+              data={resultData}
+              expandAll={true}
+              renderNode={(node) => <MarkdownComponent markdownText={node.name || "**tree node name is missing!**"} />}
+            />
+            {!!errorMessage && <span style={{ color: "red" }}>{errorMessage}</span>}
+            {debug && <JSONDataViewer metadata={resultData} title="Skeleton Raw Data Preview" />}
+          </>
+        ) : null;
+
+      default:
+        return <div style={{ whiteSpace: "pre-wrap" }}>{content}</div>;
+    }
+  };
+
   return (
     <>
-      {textOutputType === SupportedOutFormats.TEXT && <pre>{content}</pre>}
+      {renderContent()}
 
-      {textOutputType === SupportedOutFormats.HTML && (
-        <div style={{ whiteSpace: "pre-wrap" }} dangerouslySetInnerHTML={{ __html: content }} />
-      )}
-
-      {textOutputType === SupportedOutFormats.MARKDOWN && <MarkdownComponent markdownText={content} />}
-
-      {textOutputType === SupportedOutFormats.YAML && (
+      {debug && (
         <>
-          <pre>{JSON.stringify(yamlProcessedData, null, 2)}</pre>
-          {!!errorMessage && <span style={{ color: "red" }}>{errorMessage}</span>}
+          <CopyButton buttonText="Copy Skeleton As Yaml" textToCopy="" onCopy={() => {}} />
+          <JSONDataViewer metadata={{ data, resultData, errorMessage }} title="X-Ray" />
         </>
       )}
-
-      {textOutputType === SupportedOutFormats.TIS_to_SKELETON && resultData.length > 0 && (
-        <>
-          <Tree
-            data={resultData}
-            expandAll={true}
-            renderNode={(node) => <MarkdownComponent markdownText={node.name || "**tree node name is missing!**"} />}
-          />
-          {!!errorMessage && <span style={{ color: "red" }}>{errorMessage}</span>}
-          {debug && <JSONDataViewer metadata={resultData} title="Skeleton Raw Data Preview" />}
-        </>
-      )}
-
-      {!Object.values(SupportedOutFormats).includes(textOutputType) && (
-        <div style={{ whiteSpace: "pre-wrap" }}>{content}</div>
-      )}
-
-      {debug && <CopyButton buttonText="Copy Skeleton As Yaml" textToCopy="" onCopy={() => {}} />}
-      {debug && <JSONDataViewer metadata={{ data, resultData, errorMessage }} title="X-Ray" />}
     </>
   );
 };

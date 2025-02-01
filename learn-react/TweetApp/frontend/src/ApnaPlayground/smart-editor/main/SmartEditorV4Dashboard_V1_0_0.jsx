d66b4c57712586bbfe7d9c7ae6818003e2fb8758
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-01 19:44:14 +0530

    commit

--- learn-react/TweetApp/frontend/src/ApnaPlayground/smart-editor/main/SmartEditorV4Dashboard_V1_0_0.jsx
+++ learn-react/TweetApp/frontend/src/ApnaPlayground/smart-editor/main/SmartEditorV4Dashboard_V1_0_0.jsx
@@ -1,10 +1,10 @@
 import React, { useMemo, useState } from "react";
-import { SmartEditor as SmartEditorV3, SmartPreviewer as SmartPreviewerV3 } from "../../../common/components/Smart/Editor/v3";
 import SmartEditorV4 from "../../../common/components/Smart/Editor/v4";
 import { smartPreviewerDataArray as smartPreviewerData, observations } from "../common/data";
 import ToggleablePanel from "../../../common/components/toggleable-panel/ToggleablePanel";
 import CustomButton from "../../../common/components/custom-button/CustomButton";
 import JSONDataViewer from "../../../common/components/json-data-viewer/JSONDataViewer";
+import SmartPreviewerV4 from '../../../common/components/Smart/Editor/v4'
 
 const SmartEditorV4Dashboard_V1_0_0 = () => {
   const [selectedDataIndex, setSelectedDataIndex] = useState(0);
@@ -13,17 +13,17 @@ const SmartEditorV4Dashboard_V1_0_0 = () => {
     <div>
       <div>
         <b>Data title: </b> {selectedData.title} <br />
-        <ToggleablePanel showContent={true} title="observations">
-          <SmartPreviewerV3 data={{ content: observations, textOutputType: "text" }} />
+        <ToggleablePanel showContent={false} title="observations">
+          <SmartPreviewerV4 data={{ content: observations, textOutputType: "text" }} />
         </ToggleablePanel>
         <CustomButton onClick={() => setSelectedDataIndex((prev) => (prev + 1) % smartPreviewerData.length)}>Next</CustomButton>
       </div>
-      <ToggleablePanel showContent={false} title="Normal Textarea">
+      {/* <ToggleablePanel showContent={false} title="Normal Textarea">
         <textarea value={selectedData.data.content} rows={20} style={textareaStyle} />
-      </ToggleablePanel>
-      <ToggleablePanel showContent={false} title="SmartEditorV3">
+      </ToggleablePanel> */}
+      {/* <ToggleablePanel showContent={false} title="SmartEditorV3">
         <SmartEditorV3 initialValue={selectedData.data} />
-      </ToggleablePanel>
+      </ToggleablePanel> */}
       <ToggleablePanel showContent={false} title="SmartEditorV4[_unstable]">
         <SmartEditorV4 initialValue={selectedData.data} />
       </ToggleablePanel>
@@ -32,14 +32,14 @@ const SmartEditorV4Dashboard_V1_0_0 = () => {
   );
 };
 
-const textareaStyle = {
-  width: "100%",
-  padding: "10px",
-  fontSize: "16px",
-  borderRadius: "4px",
-  border: "1px solid #ccc",
-  resize: "none",
-  overflow: "auto",
-};
+// const textareaStyle = {
+//   width: "100%",
+//   padding: "10px",
+//   fontSize: "16px",
+//   borderRadius: "4px",
+//   border: "1px solid #ccc",
+//   resize: "none",
+//   overflow: "auto",
+// };
 
 export default SmartEditorV4Dashboard_V1_0_0;

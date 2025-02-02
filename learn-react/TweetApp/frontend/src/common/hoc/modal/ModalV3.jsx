8989b3ef5dd1ec9e1a0abede7a978d06a9cb9c94
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-02 13:46:36 +0530

    commit

--- learn-react/TweetApp/frontend/src/common/hoc/modal/ModalV3.jsx
+++ learn-react/TweetApp/frontend/src/common/hoc/modal/ModalV3.jsx
@@ -18,4 +18,34 @@ const ModalV3 = ({ isOpen, onClose, closeOnEscKey, showCloseButton = false, chil
   );
 };
 
+export const ModalV4 = ({ isOpen, onClose, closeOnEscKey, showCloseButton = false, children }) => {
+  if (!isOpen) return null;
+  return (
+    <div className="modal fade">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="staticBackdropLabel">
+              Modal title
+            </h1>
+            {showCloseButton && (
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            )}
+          </div>
+          <div className="modal-body">{children}</div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Understood
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default ModalV3;
+

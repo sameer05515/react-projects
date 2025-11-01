b2402a36ffd66cdc66ac79bfddfee7e30a174578
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-11-01 18:57:38 +0530

    commit

--- learn-react/TweetApp/backend/src/routes/ChatGPTConversation.routes.js
+++ learn-react/TweetApp/backend/src/routes/ChatGPTConversation.routes.js
@@ -13,36 +13,188 @@ const handleRequest = async (res, serviceFn, ...params) => {
     }
 };
 
-// Get all cgpt-files, without conversation or message info
+/**
+ * @swagger
+ * /cgpt/f:
+ *   get:
+ *     summary: Get all cgpt-files (without conversation or message info)
+ *     tags:
+ *       - ChatGPTConversation
+ *     responses:
+ *       200:
+ *         description: Array of cgpt file categories.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties: {}
+ *       500:
+ *         description: Server error
+ */
 router.get("/f", async (req, res) => {
     handleRequest(res, chatGPTConversation.getAllCategories);
 });
 
-// Get a cgpt-file for a given uniqueId
+/**
+ * @swagger
+ * /cgpt/f/{uniqueId}:
+ *   get:
+ *     summary: Get a cgpt-file for a given uniqueId
+ *     tags:
+ *       - ChatGPTConversation
+ *     parameters:
+ *       - in: path
+ *         name: uniqueId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The uniqueId of the cgpt-file category
+ *     responses:
+ *       200:
+ *         description: The cgpt-file object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       500:
+ *         description: Server error
+ */
 router.get("/f/:uniqueId", async (req, res) => {
     const { uniqueId } = req.params;
     handleRequest(res, chatGPTConversation.getCategoryForUniqueId, uniqueId);
 });
 
-// Get a cgpt-file for a given uniqueId and convUID
+/**
+ * @swagger
+ * /cgpt/f/{uniqueId}/c/{convUID}:
+ *   get:
+ *     summary: Get a cgpt-file for a given uniqueId and conversation UID
+ *     tags:
+ *       - ChatGPTConversation
+ *     parameters:
+ *       - in: path
+ *         name: uniqueId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The uniqueId of the cgpt-file category
+ *       - in: path
+ *         name: convUID
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The conversation UID
+ *     responses:
+ *       200:
+ *         description: The cgpt-file object for uniqueId and conversation UID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       500:
+ *         description: Server error
+ */
 router.get("/f/:uniqueId/c/:convUID", async (req, res) => {
     const { uniqueId, convUID } = req.params;
     handleRequest(res, chatGPTConversation.getCGPTFileForUIDAndConvUID, uniqueId, convUID);
 });
 
-// Get a cgpt-file for a given uniqueId, convUID, and msgUID
+/**
+ * @swagger
+ * /cgpt/f/{uniqueId}/c/{convUID}/m/{msgUID}:
+ *   get:
+ *     summary: Get a cgpt-file for a given uniqueId, conversation UID, and message UID
+ *     tags:
+ *       - ChatGPTConversation
+ *     parameters:
+ *       - in: path
+ *         name: uniqueId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The uniqueId of the cgpt-file category
+ *       - in: path
+ *         name: convUID
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The conversation UID
+ *       - in: path
+ *         name: msgUID
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The message UID
+ *     responses:
+ *       200:
+ *         description: The cgpt-file object for uniqueId, conversation UID, and message UID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       500:
+ *         description: Server error
+ */
 router.get("/f/:uniqueId/c/:convUID/m/:msgUID", async (req, res) => {
     const { uniqueId, convUID, msgUID } = req.params;
     handleRequest(res, chatGPTConversation.getCGPTFileForUIDAndConvUIDAndMsgUID, uniqueId, convUID, msgUID);
 });
 
-// Get a conversation for a given convUID
+/**
+ * @swagger
+ * /cgpt/c/{convUID}:
+ *   get:
+ *     summary: Get a conversation for a given conversation UID
+ *     tags:
+ *       - ChatGPTConversation
+ *     parameters:
+ *       - in: path
+ *         name: convUID
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The conversation UID
+ *     responses:
+ *       200:
+ *         description: The conversation object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       500:
+ *         description: Server error
+ */
 router.get("/c/:convUID", async (req, res) => {
     const { convUID } = req.params;
     handleRequest(res, chatGPTConversation.getConversationsForConvUID, convUID);
 });
 
-// Get a message for a given msgUID
+/**
+ * @swagger
+ * /cgpt/m/{msgUID}:
+ *   get:
+ *     summary: Get a message for a given message UID
+ *     tags:
+ *       - ChatGPTConversation
+ *     parameters:
+ *       - in: path
+ *         name: msgUID
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The message UID
+ *     responses:
+ *       200:
+ *         description: The message object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       500:
+ *         description: Server error
+ */
 router.get("/m/:msgUID", async (req, res) => {
     const { msgUID } = req.params;
     handleRequest(res, chatGPTConversation.getMessagesForMsgUID, msgUID);
